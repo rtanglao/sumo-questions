39eee29ed5163be967258daa889290432fc58f66
Roland Tanglao <roland@rolandtanglao.com>
2017-10-29 23:16:39 -0700

    add working version

--- add-questions-to-mongodb.rb
+++ add-questions-to-mongodb.rb
@@ -5,6 +5,7 @@ require 'json'
 require 'time'
 require 'date'
 require 'mongo'
+require 'CSV'
 
 # based on:
 # https://github.com/rtanglao/2016-rtgram/blob/master/backupPublicVancouverPhotosByDateTaken.rb
@@ -34,20 +35,54 @@ end
 questionsColl = db[:questions]
 questionsColl.indexes.create_one({ "id" => -1 }, :unique => true)
 
-first = true
-ARGF.each do |line|
-  # string fields have quotation marks in order to allow embedded commas, so have to remove them i guess?
+CSV.new(ARGF.file, :headers => true).each do |row|
+  # string fields have quotation marks at the beginning and end
+  # in order to allow embedded commas, so have to remove them i guess?
   # time fields are pacific time and in string format, need to convert to utc
   # integers are strings
   # last field has a "\n" so use chomp to eliminate it?
-  if first
-    first = false
-    pp line
-    next
-  end
-  pp line.split(",")
-  pp line.split(",")[2]
-  puts line.split(",")[2]
-  puts line.split(",")[8]
-  exit
+ 
+  PP::pp(row, $stderr)
+  
+
+  # "id" - 0 
+  # "title" - 1
+  # "creator_id" - 2
+  # "content" - 3
+  # "created" - 4
+  # "updated" - 5
+  # "updated_by_id" - 6
+  # "last_answer_id" - 7
+  # "num_answers", - 8
+  # "is_locked" - 9
+  # "solution_id" - 10
+  # "num_votes_past_week" 11,"locale" 12,"is_archived"13,"is_spam"14
+  # ,"marked_as_spam"15,"marked_as_spam_by_id"16,"product_id"17,"topic_id"18
+  # ,"taken_by_id"19,"taken_until"20
+  row_hash = {}
+  row_hash["id"] = row["id"].to_i
+  row_hash["title"] = row["title"]
+  row_hash["creator_id"] = row["creator_id"].to_i
+  row_hash["content"] = row["content"]
+  row_hash["created"] = Time.parse(row["created"])
+  row_hash["updated"] = Time.parse(row["updated"])
+  row_hash["updated_by_id"] =
+    row["updated_by_id"] =="NULL" ? nil : row["updated_by_id"]
+  row_hash["last_answer_id"] =
+    row["last_answer_id"] == "NULL" ? nil : row["last_answer_id"].to_i
+  row_hash["num_answers"] = row["num_answers"].to_i
+  row_hash["is_locked"] = row["is_locked"] == "0" ? false : true
+  row_hash["solution_id"] = 
+      row["solution_id"] == "NULL" ? nil : row["solution_id"].to_i
+  row_hash["num_votes_past_week"] = row["num_votes_past_week"].to_i
+  row_hash["locale"] = row["locale"]
+  row_hash["is_archived"] = row["is_archived"] == "0" ? false : true
+  row_hash["is_spam"] = row["is_spam"] == "0" ? false : true
+  row_hash["product_id"] = row["product_id"].to_i
+  row_hash["topic_id"] = row["topic_id"].to_i
+  PP::pp(row_hash, $stderr)
+  id = row_hash["id"]
+  questionsColl.find({ 'id' => id }).update_one(
+    row_hash,:upsert => true )
+
 end
